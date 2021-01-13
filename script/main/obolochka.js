279f588dea30551ecbbc9584b9098b360812d11c
Kramolnikk <lozeishik@ya.ru>
2021-01-14 05:47:05 +0700

    Новые горизонты говнокода.
    Попытаюсь понять логику setTimeout и вызова анонимных процедур, для сжатия в красивый цикл.

--- script/main/obolochka.js
+++ script/main/obolochka.js
@@ -18,3 +18,43 @@ function textgen(idel){
 
     document.getElementById(idel).innerHTML = tempStr;
 }
+
+function fire(){
+  setTimeout(() => potyshi1('mainmenu'), 1000)
+
+  function potyshi1(el){
+    document.getElementById(el).classList.add('firestep2');
+    setTimeout(() => potyshi2('fon'), 1500)
+  }
+  function potyshi2(el){
+    document.getElementById(el).classList.add('firestep2');
+    setTimeout(() => potyshi3('svitok'), 1500)
+  }
+  function potyshi3(el){
+    document.getElementById(el).classList.add('firestep2');
+    setTimeout(() => potyshi4('mainmenu'), 1500)
+  }
+
+  function potyshi4(el){
+    document.getElementById(el).classList.add('firestep3');
+    setTimeout(() => potyshi5('svitok'), 1500)
+  }
+  function potyshi5(el){
+    document.getElementById(el).classList.add('firestep3');
+    setTimeout(() => potyshi6('fon'), 1500)
+  }
+  function potyshi6(el){
+    document.getElementById(el).classList.add('firestep3');
+    setTimeout(() => destrall(), 1500)
+  }
+
+  function destrall(){
+    document.getElementById('mainmenu').style.display = 'none';
+    document.getElementById('fon').style.display = 'none';
+    document.getElementById('svitok').style.display = 'none';
+  }
+/*
+  document.getElementById('mainmenu').classList.add('firestep2');
+  document.getElementById('fon').classList.add('firestep2');
+  document.getElementById('svitok').classList.add('firestep2');*/
+}
