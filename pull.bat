9e8509c20871842b9a8f9e8e5cdcab08b23859c9
Kramolnikk <lozeishik@ya.ru>
2021-01-14 03:36:31 +0700

    Revert "задолбал кривой гит"
    This reverts commit d95e927434d8f3299e46ade8bddc854d12011177.

--- pull.bat
+++
@@ -1,3 +0,0 @@
-git remote add randomstr https://github.com/Kramolnikk/randomstr.git
-git pull randomstr master
-cmd
--- push.bat
+++
@@ -1,5 +0,0 @@
-git add -A
-git commit -m "Batnik"
-git remote add randomstr https://github.com/Kramolnikk/randomstr.git
-git push randomstr master
-cmd
