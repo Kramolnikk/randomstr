bdd4b6c961f7ea78592398d152a3c1b9a0d3a600
Kramolnikk <lozeishik@ya.ru>
2021-01-21 12:22:29 +0700

    улучшена функция сжигания

--- script/main/obolochka.js
+++ script/main/obolochka.js
@@ -16,32 +16,22 @@ function text_scroll(el){
 }
 
 function fire(){
-	var i = 0;
-  var elar = [
-    document.getElementById('fon'),
-    document.getElementById('svitok'),
-    document.getElementById('mainmenu'),
-  ];
+  var i = 0;
+  var ii = 0;
+  var elar = document.getElementById('continer').children;
   step1(elar);
 	function step1(el){
     if (i < el.length) {
       el[i].classList.add('firestep1');
 			i++;
       setTimeout(step1, 1400, el);
-    } else
-    {i--; step2(el);}
-  }
-  	function step2(el){
-    if (i >= 0) {
-      el[i].classList.add('firestep2');
-			i--;
-      setTimeout(step2, 1500, el);
-    } else {setTimeout(function() {
-      
-      el[0].style.display = 'none';
-      el[1].style.display = 'none';
-      el[2].style.display = 'none';
-    }, 1000, el);
+    }
+    else {
+      if (ii < el.length){
+        ii++;
+        el[i-ii].classList.add('firestep2');
+        setTimeout(step1, 1500, el);
+      }
     }
   }
 }
