0677dcd0e30d2b1e824a78e5da9c14016af0cefa
Kramolnikk <lozeishik@ya.ru>
2021-01-28 19:03:43 +0700

    Парсим на два уровня вложенности с переносом строки

--- script/editor/dialog.js
+++ script/editor/dialog.js
@@ -1,4 +1,6 @@
-function emptyDetect(el){
+function emptyDetect(el_){
+    var el;
+    (typeof el_ === "string" || el_ instanceof String) ? el = document.getElementById(el_) : el = el_;
     if (el.value == ""){
         el.classList.add("redfon")
     }
@@ -7,22 +9,50 @@ function emptyDetect(el){
     }
 }
 
-function smpleParse(){
+function smpleParse(el_){
+    var el;
+    (typeof el_ === "string" || el_ instanceof String) ? el = document.getElementById(el_) : el = el_;
     var strtmp = '';
+    var arrayMain = [];
     var array = [];
     var str = document.getElementById('testTextarea').value;
-    var schet = 0;
-    //str.splice(0, 1);
-    for (var i=0; i<=str.length; i++){
-        if (str[i] == '\u005C' || i == str.length){
-            array[schet] = strtmp;
+    var x = 0, y = 0;
+    for (var i=0; i<= str.length; i++){
+        if (str[i] == '\u005C' || i == str.length || str[i] == '\n'){
+            array[x] = strtmp;
             strtmp='';
-            schet++;
+            x++;
+            if (str[i] == '\n' || i == str.length) {
+                x=0;
+                arrayMain[y] = array;
+                y++;
+                array = [];
+            }
         }
         else{
             strtmp += str[i];
         }
     }
-    document.getElementById('testSpanData').innerHTML = "(" + array.length + ")";
-    document.getElementById('testData').innerHTML = array;
+    document.getElementById('testSpanData').innerHTML = '';
+    document.getElementById('testSpanData').innerHTML = "(" + arrayMain.length + ")";
+    document.getElementById('testData').innerHTML = '';
+    for (var i=0; i<= arrayMain.length-1; i++){
+        document.getElementById('testData').innerHTML += arrayMain[i] + "<br>";
+    }
 }
+/*
+
+ar =  [
+    [0..9];
+    [0..9];
+    [0..9];
+]
+    var array = [[],[]];
+        if (str[i] == '\n') {
+            y++;
+            x=0;
+            array[y][x] = strtmp;
+            strtmp='';
+            x++;
+        }
+*/
