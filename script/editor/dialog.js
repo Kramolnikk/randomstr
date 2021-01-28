461ecebed4e23febdf6c1431d22f7571fd286e0a
Kramolnikk <lozeishik@ya.ru>
2021-01-28 17:45:46 +0700

    Наброски парсера.

--- script/editor/dialog.js
+++ script/editor/dialog.js
@@ -6,3 +6,23 @@ function emptyDetect(el){
         el.classList.remove("redfon")
     }
 }
+
+function smpleParse(){
+    var strtmp = '';
+    var array = [];
+    var str = document.getElementById('testTextarea').value;
+    var schet = 0;
+    //str.splice(0, 1);
+    for (var i=0; i<=str.length; i++){
+        if (str[i] == '\u005C' || i == str.length){
+            array[schet] = strtmp;
+            strtmp='';
+            schet++;
+        }
+        else{
+            strtmp += str[i];
+        }
+    }
+    document.getElementById('testSpanData').innerHTML = "(" + array.length + ")";
+    document.getElementById('testData').innerHTML = array;
+}
